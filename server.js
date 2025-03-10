9e2f77ced6f1a425d6efe3fc188c1a84f7eb52da
osundstrom <oskar.sundstrom123@gmail.com>
2025-03-10 19:07:09 +0100

    first

---
+++ server.js
@@ -0,0 +1,46 @@
+//---------------------------Allt som krävs----------------------------------//
+//dotenv
+require("dotenv").config();
+
+//koa
+const koa = require("koa");
+
+//bodyparser
+const bodyParser = require("koa-bodyparser");
+
+//mongoose
+const mongoose = require("mongoose");
+
+//cors
+const cors = require("@koa/cors");
+
+
+
+//---------------------------Använder----------------------------------//
+
+//använder koa
+const app = new koa();
+
+
+//använd cors
+app.use(cors());
+
+//använd bodyparser
+app.use(bodyParser());
+
+
+
+//---------------------------Ansluter mongodb----------------------------------//
+
+mongoose.connect(process.env.URL)
+  .then(() => {
+    console.log("Ansluten")})
+  .catch(error => {
+    console.log("Misslyckades", error)});
+
+
+//---------------------------startar----------------------------------//
+
+app.listen(process.env.PORT, () => {
+    console.log("Startar server");
+});
